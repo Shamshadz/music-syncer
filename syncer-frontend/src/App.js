df3bdf132cb61bee2d6201b17de56a142f8c7052
shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-01-17 17:25:41 +0530

    update

--- syncer-frontend/src/App.js
+++ syncer-frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "react-dom";
 import HomePage from "./components/HomePage";
 import RoomJoinPage from "./components/RoomJoinPage";
 import CreateRoomPage from "./components/CreateRoomPage";
-import { BrowserRouter as Router, Routes, Route, Link, Redirect,} from "react-router-dom";
+import Room from "./components/Room";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 
 function App(){
@@ -12,8 +12,9 @@ function App(){
       <Router>
         <Routes>
           <Route exact path="/" element={<HomePage/>}/>
-          <Route exact path="/join" element={<RoomJoinPage/>} />
-          <Route exact path="/create" element={<CreateRoomPage/>} />
+          <Route path="/join" element={<RoomJoinPage/>} />
+          <Route path="/create" element={<CreateRoomPage/>} />
+          <Route path="/room/:code" element={<Room/>} />
         </Routes>
       </Router>
     );
---
+++ syncer-frontend/src/components/Room.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+const Room = () =>{
+
+    
+
+    return(
+        <div>
+            <p>Room Code :</p>
+            <p>Guest Can Pause: </p>
+            <p>Votes to Skip :</p>
+        </div>
+    );
+}
+
+export default Room;
